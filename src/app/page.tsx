39d948e1fe26bbf3e96b56d7af98d536deb870ab
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-02-10 16:09:43 +0530

    Add user authentication check to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,20 @@
-
+'use client';
+import { useUser } from '@clerk/nextjs'
 
 export default function Home() {
+  const { user } = useUser()
+
+  if (!user) {
+    return (
+      <div>
+        <p>You are not signed in</p>
+      </div>
+    )
+  }
+  console.log("user found", user);
+
+
+
   return (
     <div>
       home
